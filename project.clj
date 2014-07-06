5848e1d64fa293bee0ca8c4c14ec37563be8a2d0
Anders Hovmöller <boxed@killingar.net>
2014-07-06 14:58:18 +0200

    Updated leiningen version

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.3.4"
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
                  [irclj "0.5.0-alpha4"]
